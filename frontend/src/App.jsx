4e1982c3daa2f34c001f98af5f19c6fe2015a77e
Gbandi14 <gbandi.555@gmail.com>
2024-03-06 19:03:03 +0100

    TS-2 Router és fileok előkészítve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,31 @@
-import './css/App.css';
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import './css/App.css'
 
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Gallery from './pages/Gallery'
+import About from './pages/About'
+import Contacts from './pages/Contacts'
+import Offer from './pages/Offer'
+import Admin from './pages/Admin'
+
+axios.defaults.withCredentials = true
 function App() {
   return (
     <div className="text-white bg-[#2a3952] min-h-screen">
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/gallery/*" element={<Gallery />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/offer" element={<Offer />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </div>
   );
 }
