fb1ecfab7ae586be6fdf4dd691952b94ef64aeb1
Gbandi14 <gbandi.555@gmail.com>
2024-03-22 19:00:38 +0100

    TS-17 Értékelések elkezdve

---
+++ frontend/src/components/Gallery/Modal.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as faEmptyStar } from '@fortawesome/free-regular-svg-icons'
+
+function Modal(props) {
+    const [hoverStar, setHoverStar] = useState(5)
+    const [star, setStar] = useState(5)
+
+  return (
+    <div className="fixed flex items-center justify-center inset-0 z-10 bg-black/30">
+        <div className='flex flex-col items-center bg-[#0F1035] px-12 py-8 rounded-xl'>
+            <p className='font-semibold'>Mennyire voltál megelégedve a cég munkájával?</p>
+            <div className='flex mt-6' onMouseLeave={() => setHoverStar(star)}>
+                {[1, 2, 3, 4, 5].map(x => hoverStar >= x ? <FontAwesomeIcon key={x} icon={faStar} onClick={() => setStar(x)} onMouseMove={() => setHoverStar(x)} className='h-8 text-[#FFD700]'/> : <FontAwesomeIcon key={x} icon={faEmptyStar} onClick={() => setStar(x)} onMouseMove={() => setHoverStar(x)} className='h-8 text-[#FFD700]'/>)}
+            </div>
+
+            <textarea name="" id="" rows="4" placeholder='Megjegyzés' className='px-2 py-1 mt-5 rounded-lg w-full resize-none outline-none text-black'></textarea>
+            <div className='flex gap-2.5 mt-6'>
+                <button className='px-5 py-2 rounded-lg bg-[#365486]/100 hover:bg-[#365486]/75 transition-colors'>Értékelés leadása</button>
+                <button onClick={() => props.close()} className='px-5 py-2 rounded-lg bg-[#FF004D]/100 hover:bg-[#FF004D]/75 transition-colors'>Mégse</button>
+            </div>         
+        </div>
+    </div>
+  )
+}
+
+export default Modal
--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
-import { Link } from 'react-router-dom';
-import htmlParse from 'html-react-parser';
+import htmlParse from 'html-react-parser'
+import Modal from '../components/Gallery/Modal'
 
 function Gallery() {
   const [items, setItems] = useState([]);
+  const [edit, setEdit] = useState(null);
   useEffect(() => {
     setItems([
       {
+        ID: 1,
         Image: "https://picsum.photos/id/237/1920/1080",
         Text: `
           <h2 className='text-center mb-3 text-3xl'>Keresztátadó</h2>
@@ -25,15 +27,19 @@ function Gallery() {
   return (
     <div>
       <Header />
-      {items.map((item) => <div className='flex justify-center w-screen'>
-        <img src={item.Image} alt="itemImage" className='m-7 w-1/2 rounded-3xl aspect-video object-cover'/>
-        <div className='m-7 w-1/4'>
-          <div className='w-full mb-8'>
-            {htmlParse(item.Text)}
+      {
+        items.map((item) => (
+        <div key={item.ID} className='flex justify-center w-screen'>
+          <img src={item.Image} alt="itemImage" className='m-7 w-1/2 rounded-3xl aspect-video object-cover'/>
+          <div className='m-7 w-1/4'>
+            <div className='w-full mb-8'>
+              {htmlParse(item.Text)}
+            </div>
+            <button onClick={() => setEdit(item.ID)} className='px-5 py-2 rounded-lg bg-[#0F1035]/100 hover:bg-[#0F1035]/75 transition-colors mt-6'>Saját értékelés írása</button>
           </div>
-          <Link to={'/'} className='px-5 py-2 rounded-lg bg-[#0F1035]/100 hover:bg-[#0F1035]/75 transition-colors mt-6'>Saját értékelés írása</Link>
-        </div>
-      </div>)}
+        </div>))
+      }
+      {edit != null ?<Modal ID={edit} close={() => setEdit(null)} /> : null }
     </div>
   )
 }
