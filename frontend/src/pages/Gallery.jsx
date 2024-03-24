2feb0761946f68a4365494e5eb4e5500b56b177b
Gbandi14 <gbandi.555@gmail.com>
2024-03-24 11:00:08 +0100

    TS-12 Reszponzív galéria kész

--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -13,14 +13,28 @@ function Gallery() {
         Image: "https://picsum.photos/id/237/1920/1080",
         Text: `
           <h2 className='text-center mb-3 text-3xl'>Keresztátadó</h2>
-          <p className='text-justify mb-20'>Ügyfelem komplett rendszerébe illeszthető berendezés tervezése volt a cél. A feladat a raklap kétirányű mozgatása, illetve orientáció változtatás nélküli átadása másik pályára.</p>
+          <p className='text-justify-center mb-20'>Ügyfelem komplett rendszerébe illeszthető berendezés tervezése volt a cél. A feladat a raklap kétirányű mozgatása, illetve orientáció változtatás nélküli átadása másik pályára.</p>
           <h4 className='mb-3'>Feladatok:</h4>
           <ul className='list-disc ml-5'>
             <li>EUR raklap szállítása, amely maximális tömege 1 tonna</li>
             <li>Keresztátadás az orientáció megtartásával</li>
             <li>Kompakt kialakítás, amely más projektbe történő gyors implementálást tesz lehetővé</li>
             <li>Logika és szernzorozás kiépítése</li>
-          </ul>`
+          </ul>`, 
+      },
+      {
+        ID: 2,
+        Image: "https://picsum.photos/id/238/1920/1080",
+        Text: `
+          <h2 className='text-center mb-3 text-3xl'>Keresztátadó</h2>
+          <p className='text-justify-center mb-20'>Ügyfelem komplett rendszerébe illeszthető berendezés tervezése volt a cél. A feladat a raklap kétirányű mozgatása, illetve orientáció változtatás nélküli átadása másik pályára.</p>
+          <h4 className='mb-3'>Feladatok:</h4>
+          <ul className='list-disc ml-5'>
+            <li>EUR raklap szállítása, amely maximális tömege 1 tonna</li>
+            <li>Keresztátadás az orientáció megtartásával</li>
+            <li>Kompakt kialakítás, amely más projektbe történő gyors implementálást tesz lehetővé</li>
+            <li>Logika és szernzorozás kiépítése</li>
+          </ul>` 
       }
     ]);
   }, []);
@@ -29,13 +43,15 @@ function Gallery() {
       <Header />
       {
         items.map((item) => (
-        <div key={item.ID} className='flex justify-center w-screen'>
-          <img src={item.Image} alt="itemImage" className='m-7 w-1/2 rounded-3xl aspect-video object-cover'/>
-          <div className='m-7 w-1/4'>
-            <div className='w-full mb-8'>
+        <div key={item.ID} className='flex flex-col lg:flex-row lg:items-center lg:space-x-6 md:items-center'>
+          <img src={item.Image} alt="itemImage" className='m-7 rounded-3xl aspect-video object-cover lg:w-1/2 lg:order-1'/>
+          <div className='m-7 lg:w-1/2 lg:order-2'>
+            <div className='mb-8 max-w-screen-md'>
               {htmlParse(item.Text)}
             </div>
-            <button onClick={() => setEdit(item.ID)} className='px-5 py-2 rounded-lg bg-[#0F1035]/100 hover:bg-[#0F1035]/75 transition-colors mt-6'>Saját értékelés írása</button>
+            <div className='flex justify-center'>
+              <button onClick={() => setEdit(item.ID)} className='px-5 py-2 rounded-lg bg-[#0F1035]/100 hover:bg-[#0F1035]/75 transition-colors mt-6'>Saját értékelés írása</button>
+            </div>   
           </div>
         </div>))
       }
