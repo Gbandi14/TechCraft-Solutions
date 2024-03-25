241da7793302949396cc795fef08a23b9633cf9d
Gbandi14 <gbandi.555@gmail.com>
2024-03-25 20:35:34 +0100

    TS-23 Adatbázis elkezdve

---
+++ backend/sql/techcraft_solutions.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2024. Már 25. 20:29
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `techcraft_solutions`
+--
+CREATE DATABASE IF NOT EXISTS `techcraft_solutions` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+USE `techcraft_solutions`;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `categories`
+--
+
+CREATE TABLE `categories` (
+  `ID` int(11) NOT NULL,
+  `Title` varchar(512) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `offer`
+--
+
+CREATE TABLE `offer` (
+  `UserID` int(11) NOT NULL,
+  `ServiceID` int(11) NOT NULL,
+  `Description` varchar(512) NOT NULL,
+  `Type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `rating`
+--
+
+CREATE TABLE `rating` (
+  `UserID` int(11) NOT NULL,
+  `ReferenceID` int(11) NOT NULL,
+  `Score` int(11) NOT NULL,
+  `Text` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `references`
+--
+
+CREATE TABLE `references` (
+  `ID` int(11) NOT NULL,
+  `Image` varchar(256) NOT NULL,
+  `Text` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `services`
+--
+
+CREATE TABLE `services` (
+  `ID` int(11) NOT NULL,
+  `Title` varchar(256) NOT NULL,
+  `Description` varchar(512) NOT NULL,
+  `CategoryID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `Username` varchar(256) NOT NULL,
+  `CompanyName` varchar(256) DEFAULT NULL,
+  `Firstname` varchar(256) NOT NULL,
+  `Lastname` varchar(256) NOT NULL,
+  `PhoneNumber` varchar(16) DEFAULT NULL,
+  `Email` varchar(256) NOT NULL,
+  `Password` varchar(256) NOT NULL,
+  `ProfilePicture` varchar(256) NOT NULL,
+  `Rank` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- A tábla indexei `references`
+--
+ALTER TABLE `references`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- A tábla indexei `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `references`
+--
+ALTER TABLE `references`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `services`
+--
+ALTER TABLE `services`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
