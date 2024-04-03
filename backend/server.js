a9d3db8712d224f21814d17256f435fd65342293
Gbandi14 <gbandi.555@gmail.com>
2024-04-03 19:12:36 +0200

    TS-24 TS-25 TS-27 TS-33 Bejelentkezés és regisztráció véglegesítve, adatok lekérése, ajánlat és értékelés küldés

--- backend/server.js
+++ backend/server.js
@@ -36,35 +36,33 @@ async function verifyToken(req, res, next) {
     }
 }
 
-app.get("/references", async(req, res) => {
-    pool.query("SELECT * FROM `references`", (error, results, fields) => {
-        if (error) return res.status(500).send("Hiba történt!")
-        res.send(results)
-    })
-})
-
 app.post("/register", async(req, res) => {
     const { username, companyname, firstname, lastname, phone, email, password } = req.body
     if (!firstname && !lastname && !email && !password) {
         return res.status(400).send("Hibás adatok!")
     }
+    if (!/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/g.test(email)) {
+        return res.status(400).send("Hibás email formátum!")
+    }
 
     const hashedPassword = await bcrypt.hash(password, 8)
 
-    pool.query("INSERT INTO `users`(`Username`, `CompanyName`, `Firstname`, `Lastname`, `PhoneNumber`, `Email`, `Password`, `ProfilePicture`, `Rank`) VALUES ('"+username+"','"+companyname+"','"+firstname+"','"+lastname+"','"+phone+"','"+email+"','"+hashedPassword+"','dummy.png',1)", (error, results, fields) => {
+    pool.query("INSERT INTO `users`(`Username`, `CompanyName`, `Firstname`, `Lastname`, `PhoneNumber`, `Email`, `Password`, `ProfilePicture`, `Rank`) VALUES ('"+username+"','"+companyname+"','"+firstname+"','"+lastname+"','"+phone+"','"+email+"','"+hashedPassword+"','https://i.postimg.cc/SK2TYrVV/Profilepic.png',1)", (error, results, fields) => {
         if (error) return res.status(500).send("Sikertelen regisztráció!")
         res.send("Sikeres regisztráció!")
     })
 })
 
 app.post("/login", async(req, res) => {
-    const { email, password} = req.body
+    const { email, password } = req.body
     if (!email && !password) {
         return res.status(400).send("Hibás adatok!")
     }
 
     pool.query("SELECT * FROM `users` WHERE Email = '"+email+"'", async (error, results, fields) => {
         if (error) return res.status(500).send("Sikertelen bejelentkezés!")
+        if (results.length == 0) return res.status(400).send("Nem található felhasználó!")
+
         const passMatch = await bcrypt.compare(password, results[0].Password)
         if (!passMatch) return res.status(400).send("Hibás jelszó!")
         const accessToken = jwt.sign({ email:results[0].Email }, process.env.ACCESS_SECRET, { expiresIn:"7d" })
@@ -79,6 +77,61 @@ app.get("/userdata", verifyToken, async(req, res) => {
     pool.query("SELECT `ID`, `Username`, `CompanyName`, `Firstname`, `Lastname`, `PhoneNumber`, `Email`, `ProfilePicture`, `Rank` FROM `users` WHERE Email = '"+req.email+"'", (error, results, fields) => {
         if (error) return res.status(500).send("Hiba!")
         res.send(results[0])
+    })
+})
+
+app.post("/userupdate", verifyToken, async(req, res) => {
+    const { username, companyname, firstname, lastname, phone, email } = req.body
+    if (!firstname && !lastname && !email) {
+        return res.status(400).send("Hibás adatok!")
+    }
+    if (!/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/g.test(email)) {
+        return res.status(400).send("Hibás email formátum!")
+    }
+
+    pool.query("UPDATE `users` SET `Username`='"+username+"',`CompanyName`='"+companyname+"',`Firstname`='"+firstname+"',`Lastname`='"+lastname+"',`PhoneNumber`='"+phone+"',`Email`='"+email+"' WHERE email = '"+req.email+"';", (error, results, fields) => {
+        if (error) return res.status(500).send("Hiba!")
+        res.send("Sikeres profil módosítás!")
+    })
+})
+
+app.get("/references", async(req, res) => {
+    pool.query("SELECT * FROM `references`", (error, results, fields) => {
+        if (error) return res.status(500).send("Hiba történt!")
+        res.send(results)
+    })
+})
+
+app.get("/services", async(req, res) => {
+    pool.query("SELECT categories.ID as categoryId, categories.Title as categoryTitle, services.ID as serviceId, services.Title as serviceTitle, services.Description as serviceDescription  FROM `categories` INNER JOIN `services` ON `services`.`CategoryID` = `categories`.`ID`", (error, results, fields) => {
+        if (error) return res.status(500).send("Hiba történt!")
+        res.send(results)
+    })
+})
+
+app.post("/sendrate", verifyToken, async(req, res) => {
+    const { id, comment, star } = req.body
+
+    pool.query("SELECT ID FROM users WHERE Email = '"+req.email+"'", (error, results, fields) => {
+        if (error || results.length == 0) return res.status(500).send("Hiba!")
+
+        pool.query("INSERT INTO `rating`(`UserID`, `ReferenceID`, `Score`, `Text`) VALUES ('"+results[0].ID+"','"+id+"','"+star+"','"+comment+"')", (error2, results2, fields2) => {
+            if (error2) return res.status(500).send("Hiba!")
+            res.send("Értékelés elküldve!")
+        })
+    })
+})
+
+app.post("/sendoffer", verifyToken, async(req, res) => {
+    const { id, comment } = req.body
+
+    pool.query("SELECT ID FROM users WHERE Email = '"+req.email+"'", (error, results, fields) => {
+        if (error || results.length == 0) return res.status(500).send("Hiba!")
+
+        pool.query("INSERT INTO `offer`(`UserID`, `ServiceID`, `Description`, `Type`) VALUES ('"+results[0].ID+"','"+id+"','"+comment+"',1)", (error2, results2, fields2) => {
+            if (error2) return res.status(500).send("Hiba!")
+            res.send("Ajánlatkérés elküldve!")
+        })
     })
 })
 
