a1e4e4c42777ee04527b0b08d59a748a2b244be1
Gbandi14 <gbandi.555@gmail.com>
2024-04-08 20:19:29 +0200

    TS-16 Ajánlatkérés regisztráció nékül nem elérhető

--- frontend/src/pages/Offer.jsx
+++ frontend/src/pages/Offer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Header from '../components/Header'
+import { Link } from 'react-router-dom'
 import { faGear, faDesktop, faCode } from '@fortawesome/free-solid-svg-icons'
 import Category from '../components/Offer/Category'
 import axios from 'axios'
@@ -18,7 +18,7 @@ function Offer() {
           setEmail(res.data.Email)
       })
 
-      axios.get("http://localhost:8000/services").then((res) => {
+      axios.get("http://localhost:8000/services", {headers:{Authorization:`Bearer ${sessionStorage.getItem("token")}`}}).then((res) => {
         const groupData = res.data.reduce((a, c) => {
           const category = a.find(x => x.ID === c.categoryId)
           if (category) {
@@ -42,13 +42,20 @@ function Offer() {
 
   return (
     <div>
-      <Header />
-      <div>
-        <h3 className='font-semibold mx-10 my-8 text-xl'>Kérlek válassz az alábbi ágazataink közül!</h3>
-        <div className='rounded-lg overflow-hidden flex flex-col gap-0.5 mx-10'>
-          {items.map((item, i) => <Category key={i} icon={item.Icon} title={item.Title} services={item.Services} contentIndex={i + 1} openedContent={openedContent} setOpenedContent={setOpenedContent} email={email} firstname={firstname} lastname={lastname} />)}        
+      { items.length > 0 ? 
+        <div>
+          <h3 className='font-semibold mx-10 my-8 text-xl'>Kérlek válassz az alábbi ágazataink közül!</h3>
+          <div className='rounded-lg overflow-hidden flex flex-col gap-0.5 mx-10'>
+            {items.map((item, i) => <Category key={i} icon={item.Icon} title={item.Title} services={item.Services} contentIndex={i + 1} openedContent={openedContent} setOpenedContent={setOpenedContent} email={email} firstname={firstname} lastname={lastname} />)}        
+          </div>
+        </div> 
+      : 
+        <div className='bg-[#0F1035] mx-auto max-w-max mt-10 px-16 py-10 rounded-xl'>
+          <h2>Az ajánlatkéréshez regisztrációra van szükség!</h2>
+          <p className='text-center'><Link to={'/register'} ><button className='bg-[#365486]/50 hover:bg-[#365486]/75 transition-colors rounded-xl px-8 py-3 mt-10'>Regisztrálj fiókot</button></Link></p>
+          <p className='text-center'><Link to={'/login'} ><button className='bg-[#365486]/50 hover:bg-[#365486]/75 transition-colors rounded-xl px-8 py-3 mt-3'>Már rendelkezem fiókkal</button></Link></p>
         </div>
-      </div>
+      }
     </div>
   )
 }
