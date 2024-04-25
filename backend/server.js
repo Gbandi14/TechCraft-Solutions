004b47a7bad047ceeda51f680031022e26f7188f
Gbandi14 <144482879+Gbandi14@users.noreply.github.com>
2024-04-25 11:12:00 +0200

    TS-24 Sikeres bejelentkezés üzenet javítva

--- backend/server.js
+++ backend/server.js
@@ -105,7 +105,7 @@ app.post("/login", async(req, res) => {
         const accessToken = jwt.sign({ email:results[0].Email }, process.env.ACCESS_SECRET, { expiresIn:"7d" })
         
 
-        res.send({ msg:"Sikeres regisztráció!", token:accessToken })
+        res.send({ msg:"Sikeres bejelentkezés!", token:accessToken })
     })
 
 })
