8f7185a8540d8c307249d0b41802c336ea602adc
Gbandi14 <gbandi.555@gmail.com>
2024-04-30 17:53:31 +0200

    TS-23 Üres adatbázis rg felhasználóval

--- backend/sql/techcraft_solutions.sql
+++ backend/sql/techcraft_solutions.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Már 26. 18:18
+-- Létrehozás ideje: 2024. Ápr 30. 17:45
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -31,8 +31,18 @@ USE `techcraft_solutions`;
 
 CREATE TABLE `categories` (
   `ID` int(11) NOT NULL,
-  `Title` varchar(512) NOT NULL
+  `Title` varchar(512) NOT NULL,
+  `Text` varchar(1024) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `categories`
+--
+
+INSERT INTO `categories` (`ID`, `Title`, `Text`) VALUES
+(1, 'Gépészeti tervezés és műszaki tanácsadás', 'Gépészeti tervezés és műszaki tanácsadás olyan szakterület, amely a gépek és mechanikus rendszerek tervezésével, fejlesztésével és optimalizálásával foglalkozik. Ennek a területnek az a célja, hogy hatékony és megbízható gépeket tervezzen és hozzon létre különböző ipari és üzleti alkalmazásokhoz. A gépészeti tervezők és tanácsadók részt vesznek a gépek tervezésének minden folyamatában, kezdve az ötleteléstől és a tervezéstől egészen a prototípusok készítéséig és a gyártási folyamat támogatásáig. Ezek a szakemberek széles körű tudással rendelkeznek a mechanikus mérnöki alapelvekről, anyagokról és folyamatokról, valamint a legfrissebb technológiai fejlesztésekről. A gépészeti tervezés és műszaki tanácsadás kulcsszerepet játszik az ipari innovációban és a hatékony gépészeti rendszerek fejlesztésében a modern gazdaságban.'),
+(2, 'Webfejlesztés', 'A webfejlesztés a dinamikus és interaktív webalkalmazások, valamint weboldalak létrehozásának folyamata. A fejlesztés során a tervezéstől kezdve a kódoláson és tesztelésen át egészen a bevezetésig számos lépést tartalmaz. A webfejlesztők különböző eszközöket és technológiákat használnak, beleértve az HTML-t, CSS-t, JavaScriptet és különböző keretrendszereket. A cél az, hogy vonzó, könnyen használható és hatékony webes felületeket hozzanak létre, ami kulcsfontosságú a vállalkozások online jelenlétében és sikerében.'),
+(3, 'Szoftverfejlesztés', 'A szoftverfejlesztés a szoftverek tervezésének, kódolásának és tesztelésének folyamata, amelynek célja funkcionális és hatékony programok létrehozása. A fejlesztés során a szoftvermérnökök és fejlesztők tervezik és implementálják a különböző funkcionalitásokat és megoldásokat. A szoftverfejlesztés folyamata általában több lépésből áll, beleértve az igényfelmérést, tervezést, implementációt, tesztelést és karbantartást. A szoftverfejlesztők különböző programozási nyelveket és eszközöket használnak, mint például a Java, Python, C++ vagy JavaScript. A szoftverfejlesztés kulcsfontosságú szerepet játszik a modern informatikai iparágban, segítve az üzleti folyamatok hatékonyabbá tételét és az ügyfélélmény javítását.');
 
 -- --------------------------------------------------------
 
@@ -69,7 +79,8 @@ CREATE TABLE `rating` (
 CREATE TABLE `references` (
   `ID` int(11) NOT NULL,
   `Image` varchar(256) NOT NULL,
-  `Text` blob NOT NULL
+  `Title` varchar(256) NOT NULL,
+  `Text` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 -- --------------------------------------------------------
@@ -101,8 +112,16 @@ CREATE TABLE `users` (
   `Email` varchar(256) NOT NULL,
   `Password` varchar(256) NOT NULL,
   `ProfilePicture` varchar(256) NOT NULL,
-  `Rank` smallint(6) NOT NULL
+  `Rank` smallint(6) NOT NULL,
+  `VerifyToken` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`ID`, `Username`, `CompanyName`, `Firstname`, `Lastname`, `PhoneNumber`, `Email`, `Password`, `ProfilePicture`, `Rank`, `VerifyToken`) VALUES
+(1, 'rg', 'TechCraft Solutions Kft.', 'rg', 'rg', '', 'rg@tcs.hu', '$2b$08$qRHyBRBIhl30jeLD.ae94eSvGgi1xkPj0uanvIDPODju2HCK4HjRO', 'https://i.postimg.cc/SK2TYrVV/Profilepic.png', 3, '');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -155,25 +174,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT a táblához `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT a táblához `references`
 --
 ALTER TABLE `references`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT a táblához `services`
 --
 ALTER TABLE `services`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT a táblához `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Megkötések a kiírt táblákhoz
